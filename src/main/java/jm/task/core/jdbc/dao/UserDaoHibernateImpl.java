b5ef4087725e43e256349ad833f5b17767b39329
Killxw <killow02@mail.ru>
2024-02-01 20:35:40 +0300

    Вроде все подправил 1.1.5

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,8 +2,8 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -15,75 +15,122 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        String sql = """
+        Session session = null;
+        try {
+            session = Util.getSessionFactory().openSession();
+            session.beginTransaction();
+            String sql = """
                     CREATE TABLE IF NOT EXISTS users (id BIGINT NOT NULL AUTO_INCREMENT,
                       name VARCHAR(45) NOT NULL,
                       last_name VARCHAR(45) NOT NULL,
-                      age SMALLINT NOT NULL,
+                      age TINYINT NOT NULL,
                       PRIMARY KEY (id))
                     """;
-        session.createSQLQuery(sql).executeUpdate();
-        transaction.commit();
-        System.out.println("Таблица успешно создана");
-        session.close();
+            session.createSQLQuery(sql).executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("Таблица успешно создана");
+        } catch (HibernateException e) {
+            System.out.println("Произошла ошибка при создании таблицы");
+            e.printStackTrace();
+            if (session != null) session.getTransaction().rollback();
+        } finally {
+            if (session != null) session.close();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        String sql = """
-                    DROP TABLE IF EXISTS users
-                    """;
-        session.createSQLQuery(sql).executeUpdate();
-        transaction.commit();
-        System.out.println("Таблица users успешно удалена");
-        session.close();
+        Session session = null;
+        try {
+            session = Util.getSessionFactory().openSession();
+            session.beginTransaction();
+            String sql = "DROP TABLE IF EXISTS users";
+            session.createSQLQuery(sql).executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("Таблица успешно создана");
+        } catch (HibernateException e) {
+            System.out.println("Произошла ошибка при создании таблицы");
+            e.printStackTrace();
+            if (session != null) session.getTransaction().rollback();
+        } finally {
+            if (session != null) session.close();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.save(new User(name,lastName,age));
-        transaction.commit();
-        System.out.println("User с именем – " + name + " добавлен в базу данных");
-        session.close();
+        Session session = null;
+        try {
+            session = Util.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            session.getTransaction().commit();
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (HibernateException e) {
+            System.out.println("Не удалось добавить User в таблицу.");
+            if (session != null) session.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            if (session != null) session.close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        String sql = "DELETE FROM users WHERE id = " + id;
-        session.createSQLQuery(sql).executeUpdate();
-        transaction.commit();
-        System.out.println("Из таблицы users успешно удалена следующая запись с id = " + id);
-        session.close();
+        Session session = null;
+        try {
+            session = Util.getSessionFactory().openSession();
+            session.beginTransaction();
+            String sql = "DELETE FROM users WHERE id = " + id;
+            session.createSQLQuery(sql).executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("Таблица успешно создана");
+        } catch (HibernateException e) {
+            System.out.println("Произошла ошибка при создании таблицы");
+            e.printStackTrace();
+            if (session != null) session.getTransaction().rollback();
+        } finally {
+            if (session != null) session.close();
+        }
     }
 
     @Override
-
     public List<User> getAllUsers() {
-        Session session = Util.getSessionFactory().openSession();;
-        Transaction transaction = session.beginTransaction();
-        String sql = "SELECT * FROM users";
-        List<User> users  = session.createSQLQuery(sql).addEntity(User.class).list();
-        System.out.println(users.size());
-        transaction.commit();
+        Session session = null;
+        List<User> users = null;
+        try {
+            session = Util.getSessionFactory().openSession();
+            session.beginTransaction();
+            String sql = "SELECT * FROM users";
+            users = session.createSQLQuery(sql).addEntity(User.class).list();
+            System.out.println(users.size());
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            System.out.println("Произошла ошибка при создании таблицы");
+            e.printStackTrace();
+            if (session != null) session.getTransaction().rollback();
+        } finally {
+            if (session != null) session.close();
+        }
         return users;
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        String sql = "TRUNCATE users";
-        session.createSQLQuery(sql).executeUpdate();
-        transaction.commit();
-        System.out.println("Таблица users успешно очищена");
-        session.close();
+        Session session = null;
+        try {
+            session = Util.getSessionFactory().openSession();
+            session.beginTransaction();
+            String hql = "DELETE FROM User";
+            session.createQuery(hql).executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("Таблица users успешно очищена");
+        } catch (HibernateException e) {
+            System.out.println("Произошла ошибка при создании таблицы");
+            e.printStackTrace();
+            if (session != null) session.getTransaction().rollback();
+        } finally {
+            if (session != null) session.close();
+        }
     }
 }
